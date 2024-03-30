1edc5ffc263961c25130a114994efe6f5ba1a3d0
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-03-29 21:00:37 -0700

    splash screen and icon

--- qrename/app.py
+++ qrename/app.py
@@ -1,9 +1,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from .views import Window
+try:
+    import pyi_splash
+except ImportError:
+    pass
 
 def main():
     app = QApplication(sys.argv)
     win = Window()
     win.show()
+    try:
+        pyi_splash.close()
+    except:
+        pass
     sys.exit(app.exec_())
